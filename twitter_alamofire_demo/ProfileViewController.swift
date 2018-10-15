51432b694af7ef1bbc2370be7a2ad457fb2b4ea5
Tommy Tran <tommytran@Tommys-MacBook-Pro.local>
2018-10-15 15:11:15 -0700

    base layout

---
+++ twitter_alamofire_demo/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Tommy Tran on 15/10/2018.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
